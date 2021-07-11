803a4d4bd528f2465e847005bac26110604c28ef
fernandadegolin <fernandadegolin@hotmail.com>
2021-07-11 00:05:17 -0300

    construction API

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.get('/clients', function (req, res){
     res.json(data);
 });
 
+
 app.get('/clients/:id', function (req, res){
     const { id } = req.params;
     const client = data.find(cli => cli.id == id);
@@ -17,14 +18,32 @@ app.get('/clients/:id', function (req, res){
     res.json(client);
 });
 
+
 app.post('/clients', function (req, res){
     const {name, email} = req.body;
 
     res.json ({ name, email });
+});
+
+
+app.put('/clients/:id', function (req, res){
+    const { id } = req.params;
+    const client = data.find(cli => cli.id == id);
+
+    if (!client) return res.status(204).json();
+
+    const name = req.body;
+    client.name = {name};
+
+    res.json(client);
 
 });
-app.put('/clients/:id', function (req, res){});
-app.delete('/clients/:id', function (req, res){});
+
+
+app.delete('/clients/:id', function (req, res){
+    
+});
+
 
 
 app.listen(3000, function(){
